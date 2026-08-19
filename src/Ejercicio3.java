62c258c2247d020c6817e7404822a25f8ecc6a9f
camila <urreacamila@gmail.com>
2026-08-19 16:42:46 -0500

    Agrego solucion del ejercicio 3

---
+++ src/Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Ejercicio3 {
+    public static double calcularAreaRectangulo(double base, double altura) {
+        return base * altura;
+    }
+
+    public static void main(String[] args) {
+        double area = calcularAreaRectangulo(8, 5);
+        System.out.println("El area es:" + area);
+    }
+}
