e8fe3c3dbf0cbfd57d973b8c1b25c9739403f4b0
camila <urreacamila@gmail.com>
2026-08-19 16:29:17 -0500

    Agrego solucion del ejercicio 2

---
+++ src/Ejercicio2.java
@@ -0,0 +1,10 @@
+public class Ejercicio2 {
+    public static void saludarEstudiante(String nombre, int edad) {
+        System.out.println("Hola" + nombre);
+        System.out.println("tienes" + edad + "años. ¡Bienvenida :)!");
+    }
+
+    public static void main(String[] args) {
+        saludarEstudiante("Sofia", 17);
+    }
+}
