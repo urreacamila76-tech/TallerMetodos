48eefbf418c2b4aa5f64f763b84b26020de6281c
camila <urreacamila@gmail.com>
2026-08-19 16:12:24 -0500

    Agrego solucion del ejercicio1

---
+++ src/Ejercicio1.java
@@ -0,0 +1,9 @@
+public class Ejercicio1 {
+    public static void  mostrarBienvenida () {
+        System.out.println("¡Bienvenida al instituto!");
+    }
+
+    public static void main(String[] args) {
+        mostrarBienvenida();
+    }
+}
